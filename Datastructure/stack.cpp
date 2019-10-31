b7ceda870bc3e603e66056b8da468e8bb68860c7
jask <jaskirankv@gmail.com>
2019-10-31 20:28:52 +0530

    Increasing user friendliness of code

--- Datastructure/stack.cpp
+++ Datastructure/stack.cpp
@@ -1,30 +1,42 @@
-#includ<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
 	stack <int> q;
 	int x,i,n;
 	char c;
-	while(1){
+
+	while(1)
+	{
+	cout <<	"1. Enter i for pushing an element\n";
+	cout << "2. Enter d for popping an element\n";
+	cout << "3. Enter p for making the stack empty\n";
 	cin >> c;
-	if(c=='i' || c=='I'){
-		cin >> x;		
+
+	if(c=='i' || c=='I')
+	{
+		cin >> x;
 		q.push(x);
 	}
-	else if(c=='d' || c=='D'){
+	else if(c=='d' || c=='D')
+	{
 		x=q.top();
 		cout << x << endl;
 		q.pop();
 	}
-	else if(c=='p' || c=='P'){
+	else if(c=='p' || c=='P')
+	{
 		n=q.size();
-		for(i=0;i<n;i++){
+		for(i=0;i<n;i++)
+		{
 			x=q.top();
 			cout << x << endl;
 			q.pop();
 		}
 	}
- 	else if(c=='q' || c=='Q'){
+ 	else if(c=='q' || c=='Q')
+	{
  		return 0;
  	}
   }
